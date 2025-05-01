5071fafcb6ebeb95d1ead637ac99c4df7c71ef00
Savel-cmyk <savelikor@gmail.com>
2025-05-01 20:35:05 +0300

    ADDED initial project setup

---
+++ src/main/kotlin/com/transport/disabledescortserver/DisabledEscortServerApplication.kt
@@ -0,0 +1,11 @@
+package com.transport.disabledescortserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DisabledEscortServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<DisabledEscortServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/transport/disabledescortserver/DisabledEscortServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.transport.disabledescortserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DisabledEscortServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
